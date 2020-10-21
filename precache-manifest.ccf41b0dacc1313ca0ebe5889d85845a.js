c13562ed7f6b4314edef1dae21c7e2810a4039c5
Sterris <ajsterri@stud.ntnu.no>
2020-10-21 17:18:30 +0200

    Updates

---
+++ precache-manifest.ccf41b0dacc1313ca0ebe5889d85845a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd07f5207daff9b2beb0deb755ab95f3",
+    "url": "/webkurs-norkart/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs-norkart/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "6e5ac965b9783e1447e7",
+    "url": "/webkurs-norkart/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs-norkart/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs-norkart/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5ac965b9783e1447e7",
+    "url": "/webkurs-norkart/static/js/main.c8fc0365.chunk.js"
+  },
+  {
+    "revision": "9ac9a1e2cfa7fcc517eb",
+    "url": "/webkurs-norkart/static/js/runtime-main.66e63164.js"
+  }
+]);
